64914a14cc346929476f40242dadca33c8e7151e
Hilary Richardson <hilary.richardson@gmail.com>
2021-03-26 14:56:29 -0700

    syncing api routes for notes

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -8,11 +8,24 @@ router.get('/api/notes', (req, res) => {
     res.json(notes)
 })
 
-// router.get('/notes', (req, res) => {
-//     const result = 
-// })
+// post 
+// POST /api/notes should receive a new note to save on the request body,
+// add it to the db.json file, and then return the new note to the client. 
+// You'll need to find a way to give each note a unique id when it's saved 
+// (look into npm packages that could do this for you).
+router.post("/api/notes", (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
 
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send("The animal is not properly formatted.");
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
 
+});
 
 
 module.exports = router;
