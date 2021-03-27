5d232ca919bcc550fa48aac109a1f947e45cb83d
Hilary Richardson <hilary.richardson@gmail.com>
2021-03-27 11:36:41 -0700

    readability edits

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -9,15 +9,6 @@ router.get('/api/notes', (req, res) => {
     res.json(notes)
 })
 
-// write notes to database every time a note is created or deleted
-// function writeNotes(notes) {
-//     fs.writeFile("../db/db.json", JSON.stringify(notes), err => {
-//         if (err) throw err;
-//         return true;
-//     });
-// }
-
-
 // POST /api/notes should receive a new note to save on the request body,
 // add it to the db.json file, and then return the new note to the client. 
 // You'll need to find a way to give each note a unique id when it's saved 
@@ -32,14 +23,6 @@ router.post('/api/notes', (req, res) => {
     writeNotes(notes);
     // return new note as json
     res.json(res.body);
-
-    // if any data in req.body is incorrect, send 400 error back
-    // if (!validateAnimal(req.body)) {
-    //     res.status(400).send("The note is not properly formatted.");
-    // } else {
-    //     const animal = createNewAnimal(req.body, animals);
-    //     res.json(animal);
-    // }
 
 });
 
